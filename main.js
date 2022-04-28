bfd45068ebab088597c1aac46909422719d340a1
nob-iwa <103909784+nob-iwa@users.noreply.github.com>
2022-04-28 17:58:21 +0900

    修正再提出

--- main.js
+++ main.js
@@ -1,47 +1,60 @@
 /*global $ */
 $(document).ready(function(){
-  const start = document.getElementById('start');
-  const stop = document.getElementById('stop');
-  const reset = document.getElementById('reset');
+  let start = document.getElementById('start');
+  let stop = document.getElementById('stop');
+  let reset = document.getElementById('reset');
   let timeElement = document.getElementById('time');
 
-  let time=0;
-  let intervalId=null;
+  let time = 0;
+  let intervalId = null;
   
   function formatTime(time){
-    let min=Math.floor(time/60000);
-    let tsec=(Math.floor(time/10000))%60;
-    let sec=Math.floor((time%10000)/1000);
-    let msec=Math.floor((time%1000)/100);
+    let hour = Math.floor(time / 3600000);
+    let min = Math.floor(time / 60000);
+    let sec = (Math.floor(time / 1000))%60;
+    let msec = (Math.floor(time / 10))%100;
+    
+    let strhour = zeropadding(hour);
+    let strmin = zeropadding(min);
+    let strsec = zeropadding(sec);
+    let strmsec = zeropadding(msec);
   
-    return min+":"+tsec+":"+sec+":"+msec;
-  }
+    return strhour + ":" + strmin + ":" + strsec + ":" + strmsec;
+  } 
   
-  $("#start").mousedown(function(){
+  function zeropadding(value) {
+    if (value < 10) {
+      return "0" + value;
+    } else {
+      return value;
+    }
+  } 
+  
+  $("#start").click(function(){
   if(intervalId !== null){return;}
-  intervalId=setInterval(()=>{
-    time += 100;
-    timeElement.innerHTML=formatTime(time);
-  },100);
+  intervalId = setInterval(() => {
+    time += 10;
+    timeElement.innerHTML = formatTime(time);
+  },10);
    $("#start").prop("disabled", true);
    $("#stop").prop("disabled",false);
    $("#reset").prop("disabled",true);
   });
   
-  $("#stop").mousedown(function(){
+  $("#stop").click(function(){
   clearInterval(intervalId);
-  intervalId=null;
+  intervalId = null;
    $("#start").prop("disabled", false);
    $("#stop").prop("disabled",true);
    $("#reset").prop("disabled",false);
   });
   
-  $("#reset").mousedown(function() {
-  time=0;
-  timeElement.innerHTML="0:0:0:0";
+  $("#reset").click(function() {
+  time = 0;
+  timeElement.innerHTML = "00:00:00:00";
    $("#start").prop("disabled", false);
    $("#stop").prop("disabled",true);
    $("#reset").prop("disabled",true);
-  })
+  });
 
 });
